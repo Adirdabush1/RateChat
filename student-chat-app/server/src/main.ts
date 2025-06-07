c1fb4b9f51cc7e5b9d4fe3adb713e43ea68866c6
Adirdabush1 <adirdabosh33@gmail.com>
2025-06-07 23:47:38 +0300

    ajklnk

--- student-chat-app/server/src/main.ts
+++ student-chat-app/server/src/main.ts
@@ -1,19 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // רק דומיין הפרודקשן מותר
-  const allowedOrigins = [
-    'https://ratechat2.onrender.com',
-  ];
+  const allowedOrigins = ['https://ratechat2.onrender.com'];
 
   app.enableCors({
     origin: (origin, callback) => {
-      // לאפשר בקשות ללא origin (למשל Postman)
       if (!origin) return callback(null, true);
-      // לאפשר רק לדומיין הפרודקשן
       if (allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
@@ -23,8 +19,11 @@ async function bootstrap() {
     credentials: true,
   });
 
+  
+  app.useWebSocketAdapter(new IoAdapter(app));
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  console.log(`Application is running on port ${port}`);
+  console.log(`✅ Application is running on port ${port}`);
 }
 bootstrap();
