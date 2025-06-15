64b40a6f192d08a39be73f43dd1d2a1e05a12866
Adirdabush1 <adirdabosh33@gmail.com>
2025-06-15 22:01:46 +0300

    new css

--- student-chat-app/client/sudent-chat-client/src/components/Chat/ChatLobby.tsx
+++ student-chat-app/client/sudent-chat-client/src/components/Chat/ChatLobby.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getToken } from '../utils/token';
-import './styles/chatLobby.css';
+import "../pages/styles/chatLobby.css";
 
 export default function ChatLobby() {
   const [groups, setGroups] = useState<string[]>([]);
