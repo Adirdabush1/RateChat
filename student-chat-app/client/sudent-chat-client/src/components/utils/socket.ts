46988c074ec667dbde3ae9e84a2454495f074243
Adirdabush1 <adirdabosh33@gmail.com>
2025-06-15 22:12:02 +0300

    soket fix

--- student-chat-app/client/sudent-chat-client/src/components/utils/socket.ts
+++ student-chat-app/client/sudent-chat-client/src/components/utils/socket.ts
@@ -5,7 +5,6 @@ let socket: Socket | null = null;
 export const connectSocket = (token: string, CHAT_ID: string) => {
     console.log('Connecting socket with:', { token, CHAT_ID });
   socket = io('https://ratechat-1.onrender.com', {
-      transports: ['websocket'], 
     auth: {
       token,
     CHAT_ID,  
