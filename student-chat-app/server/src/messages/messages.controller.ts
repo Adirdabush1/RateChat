c7c3ce153bda987b395b346bae3656665ffd2b94
Adirdabush1 <adirdabosh33@gmail.com>
2025-06-15 20:35:54 +0300

    blabla

--- student-chat-app/server/src/messages/messages.controller.ts
+++ student-chat-app/server/src/messages/messages.controller.ts
@@ -1,26 +1,22 @@
-import { Controller, Get, Post, Delete,Body,Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { MessagesService } from './messages.service';
-
 
 @Controller('messages')
 export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
 
-
-@Get(':CHAT_ID')
-async getMessages(@Param('CHAT_ID') CHAT_ID: string) {
-  return this.messagesService.getMessagesByChat(CHAT_ID);
-}
+  @Get(':chatId')
+  async getMessages(@Param('chatId') chatId: string) {
+    return this.messagesService.getMessagesByChat(chatId);
+  }
 
   @Post()
-async createMessage(@Body() body: { CHAT_ID: string; sender: string; message: string }) {
-  return this.messagesService.saveMessage(
-    body.sender,
-    body.message,
-    body.CHAT_ID,
-    0
-  );
-}
-
-
+  async createMessage(@Body() body: { chatId: string; sender: string; message: string }) {
+    return this.messagesService.saveMessage(
+      body.sender,
+      body.message,
+      body.chatId,
+      0
+    );
+  }
 }
